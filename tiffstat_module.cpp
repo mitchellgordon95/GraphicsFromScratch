ee93c1132c8cd0af4e015fe9a84f07c50c1995c0
Mitchell Gordon <mitchell.gordon95@gmail.com>
2015-02-10 12:13:13 -0600

    Refactored tiffstat and partially parsed IFD

--- tiffstat_module.cpp
+++ tiffstat_module.cpp
@@ -7,7 +7,6 @@
 #include <cstddef>
 #include <exception>
 #include <stdexcept>
-#include <fstream>
 #include "endian_stream_reader.h"
 
 CLI_TiffStat::~CLI_TiffStat()
@@ -23,22 +22,44 @@ void CLI_TiffStat::execute(std::vector<char *> &params)
         throw std::invalid_argument(std::string("Could not read file ") + params[0]);
     }
 
+    parseTiffMeta(file, true);    
+}
+
+std::map<uint16_t, IFD_Entry> CLI_TiffStat::parseTiffMeta(std::ifstream &file, bool verbose) {
     char endianess[3];
     file.read(endianess, 2);
     endianess[2] = '\0';
 
-    std::cout << "Endianess: " << endianess << std::endl; 
     
     bool littleEndian = (strncmp(endianess, "II", 2) == 0); 
     EndianStreamReader reader(littleEndian);
 
     uint16_t magicNo = reader.getShort(file); 
-    std::cout << "Magic Number: " << magicNo << std::endl;
 
     uint32_t ifd_address = reader.getInt(file);
-    std::cout << "IFD Address: " << ifd_address << std::endl;
 
     file.seekg(ifd_address, std::ios::beg);
     uint16_t ifd_count = reader.getInt(file); 
-    std::cout << "IFD Count: " << ifd_count << std::endl;
+
+    if(verbose) {
+        std::cout << "Endianess: " << endianess << std::endl; 
+        std::cout << "Magic Number: " << magicNo << std::endl;
+        std::cout << "IFD Address: " << ifd_address << std::endl;
+        std::cout << "IFD Count: " << ifd_count << std::endl;
+    }    
+
+    std::map<uint16_t, IFD_Entry> entries;
+
+    for (int i = 0 ; i < ifd_count; ++i) {
+        IFD_Entry entry;
+        entry.tag = reader.getShort(file);
+        entry.type = reader.getShort(file);
+        entry.count = reader.getInt(file);
+        entry.value = reader.getInt(file);
+
+        
+    }    
+
+    return entries;
 }
+
--- tiffstat_module.h
+++ tiffstat_module.h
@@ -2,6 +2,22 @@
 #define TIFFSTAT_MODULE_H 
 
 #include "cli_module.h"
+#include <stdint.h>
+#include <fstream>
+#include <map>
+
+// A struct for holding an IFD entry in TIFF files
+struct IFD_Entry {
+    uint16_t tag;
+    uint16_t type;
+    uint32_t count;
+    uint32_t value;
+};
+
+// The possible value types for a TIFF IFD entry
+enum Tiff_Value_Type { BYTE=1, ASCII=2, SHORT=3, LONG=4, RATIONAL=5, SBYTE=6,
+                       UNDEFINED=7, SSHORT=8, SLONG=9, SRATIONAL=10, FLOAT=11,
+                       DOUBLE=12 };
 
 // Implements the draw command for the cli
 class CLI_TiffStat : public CLI_Module 
@@ -12,6 +28,10 @@ private:
 public:
     CLI_TiffStat(): CLI_Module("tiffstat", 1) {};
     ~CLI_TiffStat();
+
+    // Given a file, parse the TIFF IFD into structs
+    // Can optionally be verbose and print metadata to console
+    std::map<uint16_t, IFD_Entry> parseTiffMeta(std::ifstream &file, bool verbose);
 };
 
 #endif
