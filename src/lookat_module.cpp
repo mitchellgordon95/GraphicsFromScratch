6cc16c7a67a057418813b496b29c03dcda263d97
Mitchell Gordon <mitchell.gordon95@gmail.com>
2015-03-26 14:13:37 -0500

    Implemented rotate, lookat, vertex

--- src/lookat_module.cpp
+++ src/lookat_module.cpp
@@ -1,9 +1,20 @@
 #include "lookat_module.h"
-
+#include "geometry_utils.h"
 CLI_Lookat::~CLI_Lookat()
 {
 }
 
 void CLI_Lookat::execute(std::vector<char *> &params)
 {
+	float fx = CLI_Module::parseNumericalArg(params[0]);
+	float fy = CLI_Module::parseNumericalArg(params[1]);
+	float fz = CLI_Module::parseNumericalArg(params[2]);
+	float ax = CLI_Module::parseNumericalArg(params[3]);
+	float ay = CLI_Module::parseNumericalArg(params[4]);
+	float az = CLI_Module::parseNumericalArg(params[5]);
+	float ux = CLI_Module::parseNumericalArg(params[6]);
+	float uy = CLI_Module::parseNumericalArg(params[7]);
+	float uz = CLI_Module::parseNumericalArg(params[8]);
+
+	CLI_Geometry::gtLookAt(fx, fy, fz, ax, ay, az, ux, uy, uz);
 }
--- src/vertex_module.cpp
+++ src/vertex_module.cpp
@@ -1,4 +1,6 @@
 #include "vertex_module.h"
+#include "geometry_utils.h"
+#include <iostream>
 
 CLI_Vertex::~CLI_Vertex()
 {
@@ -6,4 +8,11 @@ CLI_Vertex::~CLI_Vertex()
 
 void CLI_Vertex::execute(std::vector<char *> &params)
 {
+	float x = CLI_Module::parseNumericalArg(params[0]);
+	float y = CLI_Module::parseNumericalArg(params[1]);
+	float z = CLI_Module::parseNumericalArg(params[2]);
+
+	CLI_Geometry::gtVertex3f(x, y, z);
+
+	std::cout << "Vertex at x=" << x << ",y=" << y << ",z=" << z << std::endl;;
 }
